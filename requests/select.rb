c5ddabb08af91f19105e843e1f1c4e66140d805b
Cedric <cedric.menanteau@epsi.fr>
2020-01-29 11:38:39 +0100

    Sequel request

--- requests/select.rb
+++ requests/select.rb
@@ -1,15 +1,16 @@
 # frozen_string_literal: true
 
+require 'sequel'
 require 'mysql2'
 load 'requests/conf.rb'
 
 
 Bdd = Base.new
 Bdd.init
-OpenConnectBdd = Mysql2::Client.new(:host => Bdd.host,
-                                    :database => Bdd.database,
-                                    :user => Bdd.user)
+OpenConnectBdd = Sequel.connect(adapter: 'mysql2', host: Bdd.host, database: Bdd.database, user: Bdd.user, password: Bdd.pass)
+
 def getUsers
-  query = OpenConnectBdd.query('SELECT * FROM personne')
-  query
+  result_objects = OpenConnectBdd["SELECT * FROM personne"].all
+  hash = result_objects.collect { |row| row.to_h }
+  hash.to_json
 end
