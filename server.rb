ab0776fc7a9b422d12a45c7b9c3f75542b61ecc7
Cedric <cedric.menanteau@epsi.fr>
2020-05-08 10:13:25 +0200

    update

--- server.rb
+++ server.rb
@@ -5,26 +5,42 @@ load 'requests/select.rb'
 
 Sinatra::Application.environment == :production
 
+# ROUTE : {GET}/globalMatiere
+# RETURN : JSON of Hours by Matiere
 get '/globalMatiere' do
   getChartGlobalMat
 end
 
+# ROUTE : {GET}/globalParticipation
+# RETURN : JSON of Participation by Matiere
 get '/globalParticipation' do
   getChartGlobalPart
 end
 
+# ROUTE : {GET}/promoHeuresMatieres/{intitulePromo}
+# PARAM : STRING -> Intitule Promo
+# RETURN : JSON of Hours by Matiere and by Promo
 get '/promoHeuresMatieres/:intitule' do |i|
   getHeuresMatiere(i)
 end
 
+# ROUTE : {GET}/promoPartInsc/{intitulePromo}
+# PARAM : STRING -> Intitule Promo
+# RETURN : JSON of Inscriptions and Participations by Promo
 get '/promoPartInsc/:intitule' do |i|
   getPartInsc(i)
 end
 
+# ROUTE : {GET}/promoPartMois/{intitulePromo}
+# PARAM : STRING -> Intitule Promo
+# RETURN : JSON of Participation by Month by Promo
 get '/promoPartMois/:intitule' do |i|
   getPartMois(i)
 end
 
+# ROUTE : {GET}/promoPercentMat/{intitulePromo}
+# PARAM : STRING -> Intitule Promo
+# RETURN : JSON of Percent Participation by Promo
 get '/promoPercentMat/:intitule' do |i|
   getPercentMat(i)
 end
