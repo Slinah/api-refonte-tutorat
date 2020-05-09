06253b69225d214424f9895c39e8023beae718e3
Cedric <cedric.menanteau@epsi.fr>
2020-05-09 13:11:26 +0200

    Add an import

--- server.rb
+++ server.rb
@@ -4,5 +4,6 @@ require 'sinatra'
 load 'controller/charts.rb'
 load 'controller/courses.rb'
 load 'controller/logs.rb'
+load 'controller/proposals.rb'
 
 Sinatra::Application.environment == :development
