28a1ea529971491215d20bfc9a1e19d78016f006
Cedric <cedric.menanteau@epsi.fr>
2020-05-28 14:38:29 +0200

    Initule

--- requests/courses.rb
+++ requests/courses.rb
@@ -7,7 +7,7 @@ load 'requests/promo.rb'
 
 
 def getUnclosedCourses
-  request_object = OpenConnectBdd.query('SELECT c.intitule AS intiule, c.heure AS heure, c.date AS date, c.salle AS salle, m.intitule AS matiere, po.promo AS promo, p.nom AS nom, p.prenom AS prenom FROM cours c JOIN matiere m ON c.id_matiere=m.id_matiere JOIN promo po ON c.id_promo=po.id_promo JOIN personne_cours pc ON c.id_cours=pc.id_cours JOIN personne p ON pc.id_personne=p.id_personne WHERE c.status = 0 AND pc.rang_personne = 1')
+  request_object = OpenConnectBdd.query('SELECT c.intitule AS intitule, c.heure AS heure, c.date AS date, c.salle AS salle, m.intitule AS matiere, po.promo AS promo, p.nom AS nom, p.prenom AS prenom FROM cours c JOIN matiere m ON c.id_matiere=m.id_matiere JOIN promo po ON c.id_promo=po.id_promo JOIN personne_cours pc ON c.id_cours=pc.id_cours JOIN personne p ON pc.id_personne=p.id_personne WHERE c.status = 0 AND pc.rang_personne = 1')
   hash = request_object.each(&:to_h)
   if hash.length.zero?
     'Pas de cours à venir.'
@@ -20,7 +20,7 @@ def getCourseOfASpecificUser(lastname, firstname)
   if vExistsPersonne(lastname, firstname).length.zero?
     'Cette personne n\'existe pas dans notre base.'
   else
-    request_object = OpenConnectBdd.prepare('SELECT c.intitule AS initule, c.heure AS heure, c.date AS date, c.salle AS salle, m.intitule AS matiere, po.promo AS promo, pc.rang_personne AS rang FROM cours c JOIN matiere m ON c.id_matiere=m.id_matiere JOIN promo po ON c.id_promo=po.id_promo JOIN personne_cours pc ON c.id_cours=pc.id_cours JOIN personne p ON pc.id_personne=p.id_personne WHERE c.status = 0 AND p.nom=? AND p.prenom=?')
+    request_object = OpenConnectBdd.prepare('SELECT c.intitule AS intitule, c.heure AS heure, c.date AS date, c.salle AS salle, m.intitule AS matiere, po.promo AS promo, pc.rang_personne AS rang FROM cours c JOIN matiere m ON c.id_matiere=m.id_matiere JOIN promo po ON c.id_promo=po.id_promo JOIN personne_cours pc ON c.id_cours=pc.id_cours JOIN personne p ON pc.id_personne=p.id_personne WHERE c.status = 0 AND p.nom=? AND p.prenom=?')
     request_object = request_object.execute(lastname, firstname)
   end
   hash = request_object.each(&:to_h)
