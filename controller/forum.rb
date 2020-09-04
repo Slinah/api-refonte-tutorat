703dbbfa6e899f41d931a058874e068326758f3d
bréval <breval2000@live.fr>
2020-09-04 16:51:14 +0200

    forum thinking

--- controller/forum.rb
+++ controller/forum.rb
@@ -4,27 +4,64 @@
 load 'requests/forum.rb'
 
 
+
+#
+#  Liste des question par ordre chronologique
+#   (possibilite de trie par nombre de vote)
+# Il faut donc une func qui recup la liste des question : / Titre, content, autheur, matiere, nb de vote , nb de com/
+# puis quand clique sur un post :
+# acces au details des commentaire avec possibilité de repondre
+#
+#
+#
+
+
+
+
+
+
 # ROUTE : {GET}/bot/unclosedCourses
 # RETURN : JSON of Tuteur, matière, unclosed courses
-get '/api/unclosedCourses' do
+get '/api/getForumQuestions' do
   getUnclosedCourses
 end
+
+
+
+# RETURN : JSON of Tuteur, matière, unclosed courses
+get '/api/getQuestion/:id_question' do |id_question|
+  getUnclosedCourses
+end
+
+
+# RETURN : JSON of Tuteur, matière, unclosed courses
+get '/api/getForumQuestions' do
+  getUnclosedCourses
+end
+
 
 
 # ROUTE : {POST}/bot/peopleCourse
 # PARAM : STRING -> lastname(nom) ~/~ STRING -> firstname(prénom)
 # RETURN : JSON of courses for a specified people
 # EXAMPLE : /bot/peopleCourse?lastname=Menanteau&firstname=Cédric
-post '/api/peopleCourse' do
+post '/api/createForumQuestion' do
   getCourseOfASpecificUser(params[:lastname], params[:firstname])
 end
 
+# ROUTE : {POST}/bot/peopleCourse
+# PARAM : STRING -> lastname(nom) ~/~ STRING -> firstname(prénom)
+# RETURN : JSON of courses for a specified people
+# EXAMPLE : /bot/peopleCourse?lastname=Menanteau&firstname=Cédric
+post '/api/postComQuestion' do
+  getCourseOfASpecificUser(params[:lastname], params[:firstname])
+end
 
-# ROUTE : {POST}/bot/unclosedCoursesPromo
-# PARAM : STRING -> name_promo(intitule promo)
-# RETURN : JSON of courses for a specified promo
-# EXAMPLE : /bot/unclosedCoursesPromo/B1
-post '/api/unclosedCoursesPromo/:name_promo' do |name_promo|
-  getUnclosedCoursesByIntitule(name_promo)
+# ROUTE : {POST}/bot/peopleCourse
+# PARAM : STRING -> lastname(nom) ~/~ STRING -> firstname(prénom)
+# RETURN : JSON of courses for a specified people
+# EXAMPLE : /bot/peopleCourse?lastname=Menanteau&firstname=Cédric
+post '/api/replyComQuestion' do
+  getCourseOfASpecificUser(params[:lastname], params[:firstname])
 end
 
