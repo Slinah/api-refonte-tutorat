ff5bd4400d0c370df2f9445a3bcdbf199aba4a35
lightin <breval2000@live.fr>
2020-09-07 17:04:57 +0200

    Fix comment bug

--- requests/forum.rb
+++ requests/forum.rb
@@ -6,7 +6,11 @@ load 'requests/conf.rb'
 
 
 def getForumQuestions
-  request_object = OpenConnectBdd.query('SELECT q.titre, q.description,p.prenom,m.intitule,q.status,COUNT(v.id_vote) as \'votes\',(SELECT Count(*) FROM question_forum LEFT JOIN comment on comment.id_question=question_forum.id_question  WHERE question_forum.id_question=q.id_question ) as \'comments\' FROM `question_forum` as q join personne as p on q.id_personne=p.id_personne join matiere as m on m.id_matiere=q.id_matiere LEFT join vote as v on v.id_question=q.id_question left JOIN `comment` as c  on c.id_question=q.id_question GROUP BY q.id_question')
+  request_object = OpenConnectBdd.query('SELECT q.titre, q.description,p.prenom,m.intitule,q.status,COUNT(v.id_vote)
+as \'votes\',(SELECT Count(*) FROM question_forum LEFT JOIN comment on comment.id_question=question_forum.id_question
+ WHERE question_forum.id_question=q.id_question ) as \'comments\' FROM `question_forum` as q join personne as p on
+q.id_personne=p.id_personne join matiere as m on m.id_matiere=q.id_matiere LEFT join vote as v on v.id_question=q.id_question left JOIN `comment`
+as c  on c.id_question=q.id_question GROUP BY q.id_question')
   hash = request_object.each(&:to_h)
   if hash.length.zero?
     'Impossible d\'acceder aux questions.'
