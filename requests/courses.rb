4f969d9fa471b92e92655186720454af92838783
Kraystone <leo.catifait@epsi.fr>
2020-09-09 22:15:05 +0200

    modify road getLatestCourse

--- requests/courses.rb
+++ requests/courses.rb
@@ -63,7 +63,7 @@ def getNbrOfCourse
 end
 
 def getLatestCourse
-  request_object = OpenConnectBdd.query('SELECT c.id_cours, c.heure, c.date, m.intitule as nomMatiere, c.intitule as nomCours, p.intitule as nomPromo FROM cours c join matiere m on c.id_matiere=m.id_matiere join promo p on c.id_promo=p.id_promo ORDER BY c.date DESC LIMIT 1')
+  request_object = OpenConnectBdd.query('SELECT l.id_cours, c.heure, c.date, m.intitule as nom Matiere, c.intitule as nom Cours, p.intitule as nomPromoFROM logs l JOIN cours c on c.id_cours=l.id_cours JOIN matiere m on c.id_matiere=m.id_matiere JOIN promo p on c.id_promo=p.id_promo ORDER BY c.date DESC LIMIT 1')
   hash = request_object.each(&:to_h)
     hash.to_json
 end
