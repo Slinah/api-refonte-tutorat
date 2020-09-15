ed6e2dbc5f25d97dc135ad34248954f70a817b3e
Andy <cinquin.andy@gmail.com>
2020-09-15 16:53:58 +0200

    Ajouts de routes dans courses pour la modif & la creation + petits fixs
    modif dans coursesController.rb

--- controller/coursesController.rb
+++ controller/coursesController.rb
@@ -9,6 +9,12 @@ get '/api/peopleCourseById' do
   getPeopleCourseById(params[:idPeople])
 end
 
+# ROUTE : {GET}/bot/peopleCourseById
+# RETURN : JSON of all courses for specific people for tutor
+get '/api/peopleTutorCourseById' do
+  getPeopleTutorCourseById(params[:idPeople])
+end
+
 # Insertion dans personne cours du couple id_cours & id_personne, pour un élève , si il n'existe pas déjà
 # ROUTE : {POST}/api/registrationCourse
 # PARAM : STRING -> idPeople(id personne qui s'inscrit) ~/~ STRING -> idCourse(id du cours où la personne s'inscrit)
@@ -21,6 +27,14 @@ end
 # PARAM : String : id_personne (uuid) // String : id_matiere (uuid) // String : id_promo (uuid) // String : intitule // timestamp : date // String : commentaires
 post '/api/postCourse' do
   postCourse(params[:id_personne], params[:id_matiere], params[:id_promo], params[:intitule], params[:date], params[:commentaires] )
+end
+
+
+# modification complète du cours
+# ROUTE : {POST}/api/postModifCourse
+# PARAM : tous les paramètres correspondant aux cours
+post '/api/postModifCourse' do
+  postModifCourse(params[:id_cours],params[:id_matiere],params[:id_promo],params[:intitule],params[:date],params[:commentaires],params[:nbParticipants],params[:duree],params[:status],params[:salle])
 end
 
 # ROUTE : {GET}/bot/unclosedCourses
